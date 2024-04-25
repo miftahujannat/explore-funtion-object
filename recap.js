ba22c7ff8391997d0293208eb15f8342ef7bd5ce
miftahujannat <miftahurimny@gmail.com>
2024-04-25 13:37:28 +0600

    aded

---
+++ recap.js
@@ -0,0 +1,32 @@
+var favoritebook = '4 hour work week';
+
+// array 
+var booklist = ['postinong', 'hooked', 'start with why', 'shoe dog'];
+var shoebogindex = booklist.indexOf('shoe dog');
+booklist[1] = 'story brand';
+
+booklist.push('small giants');
+booklist.pop();
+
+// conditions 
+if(booklist[1] == 'hooked'){
+    console.log('I am hooked');
+}
+else{
+    console.log('I am not hooked');
+}
+
+// while loop
+var i =0;     //loop variable 
+while (i < 15){
+    console.log(i);
+    console.log('looping looping looping');
+    i++;
+}
+
+// for loop 
+for(var i = 0; i < 15; i++;){
+    console.log(i);
+}
+
+
