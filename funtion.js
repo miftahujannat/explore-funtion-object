17c82e6493f2278d28a097a7e813fcadac88e6a2
miftahujannat <miftahurimny@gmail.com>
2024-04-25 18:58:41 +0600

    added

---
+++ funtion.js
@@ -0,0 +1,12 @@
+// funtion declaration 
+function startfan(){
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+// call the fantion 
+startfan();
+
+console.log('eat breckfast');
+console.log('drink tea');
+console.log('take a shawer');
+startfan();
