438a993c5289a921766a27d6a8cd852b9e44c8da
miftahujannat <miftahurimny@gmail.com>
2024-04-30 09:39:52 +0600

    added

---
+++ maltiple-parameter.js
@@ -0,0 +1,24 @@
+function addTowNumber(number1,number2){
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+
+var firstnumber = 32;
+var secondnumber = 47;
+var result = addTowNumber(secondnumber , firstnumber);
+console.log('result value:', result);
+
+
+
+function multiplytownumber(num1 , num2){
+    var result = num1 * num2;
+    return result;
+
+}
+var total = multiplytownumber(5,100);
+console.log('total after multiplycation', total);
+
+// biyog korar function 
+// vagfol ber koro 
+
