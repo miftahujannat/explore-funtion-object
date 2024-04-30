b114ab23039dbd1c6bdbd9498dd086e92cf7649f
miftahujannat <miftahurimny@gmail.com>
2024-04-30 10:01:00 +0600

    added

---
+++ object.js
@@ -0,0 +1,18 @@
+var student1 = {id: 115, 
+    name:'sabbir',
+    marks:81, 
+    phone:987564
+}
+var mobile = {
+    color: 'black',
+    price: 19857,
+    screensize: 5,
+    Storage: '16gb'
+};
+var book = {
+    title: 'start with why',
+    author: 'simonn sinnek',
+    price: 200,
+    pages: 245,
+    publishar: 'penguin'
+};
