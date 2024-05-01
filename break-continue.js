83a5d126247fd577c78170c6ccc4596f28f96854
miftahujannat <miftahurimny@gmail.com>
2024-05-01 10:32:56 +0600

    added

--- break-continue.js
+++ break-continue.js
@@ -1,9 +1,21 @@
-var i = 0;
-while(i < 10){
-    console.log(i);
-    if(i == 5) {
-        break;
-    }
-    i++;
+// var i = 0;
+// while(i < 10){
+//     console.log(i);
+//     if(i == 5) {
+//         break;
+//     }
+//     i++;
 
+// }
+// for (var i = 0; i < 20; i++){
+//     console.log(i);
+//     if(i > 8){
+//         break;
+//     }
+// }
+
+var number = [12, 37, 55, 67, 89, 123, 100, 102, 95];
+for(var i = 0; i < number.length; i++){
+    var number = number[i];
+    console.log(number);
 }
---
+++ summry.js
@@ -0,0 +1,33 @@
+var skycolor ='white';
+
+var phone = ['iphone', 'showmi', 'samsung', 'lg', 'htc'];
+// kono element add kortey 
+phone[3] = 'walton';
+// check an element exists in an array 
+if(phone.indexOf('oppo') -- -1){
+    console.log('opps amir khan oppo is missing');
+
+}
+
+if(phone.indexOf('lg') != -1){
+    console.log('lg is available now');
+}
+
+// loop
+var num = 0;
+while(num <- 10){
+    num++;
+}
+for(var i = 0; i < phone.length; i++){
+    console.log(i);
+}
+
+// function
+function addthreenumber(num1, num2, num3){
+    var total = num1 + num2 + num3;
+    return total;
+}
+addthreenumber(87, 41, 56);
+
+// object 
+var 
